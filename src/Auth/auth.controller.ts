7dc1978d377f30c657e71d5db73ef12af11552bf
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-11-17 12:12:07 +0500

    Prisma Module

--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
@@ -6,7 +6,8 @@ export class AuthController {
     constructor(private authService: AuthService) {    }
 
     @Post('/signup')
-    signup() {
+    signup(@Req() req: Request) {
+        console.log(req.body);
         return this.authService.signup();
     }
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './Auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, PrismaModule],
 })
 export class AppModule {}
