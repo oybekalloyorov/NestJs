a5a4a21d57d97e5fbe7578c75fe5be7e45f074ac
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-11-18 09:58:10 +0500

    Nest js Global Pipe

--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -1,19 +1,14 @@
 import { Body, Controller, ParseIntPipe, Post} from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import type { AuthDto } from "./dto";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {    }
 
     @Post('/signup')
-    signup(@Body('email') email: string, @Body('password', ParseIntPipe) password: string) {
-        console.log({
-            email,
-            typeOfEmail: typeof email,
-            password,
-            typeOfPassword: typeof password,
-        });
+    signup(@Body() dto: AuthDto) {
+        console.log(dto);
         return this.authService.signup();
     }
 
